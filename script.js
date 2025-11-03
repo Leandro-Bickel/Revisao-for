ef22080cb6ab70dc282ca6147593166e9b02f5c5
Leandro <leandrolgodoi@gmail.com>
2025-11-03 08:38:14 -0300

    inicial
    commit inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+//Exercício 1 — Números de 1 a N//
+//Peça um número e mostre todos os números de 1 até ele.//
+
+let numero = prompt("Digite um número:")
+for (let i = 1; i <= numero; i ++) {
+    console.log(i);
+}
+
+//Exercício 2 — Soma dos N primeiros números
+//Peça um número e mostre a soma de 1 até ele.
